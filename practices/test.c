3618fa23fa76c1aec4b0533c37899b9d7c0010a4
Macpotty <645092284@qq.com>
2016-03-12 20:00:07 +0800

    update

--- practices/test.c
+++ practices/test.c
@@ -1,8 +1,16 @@
-#include "stdio.h"
-#include "time.h"
-int main(){
-    int i;
-    for(i = 0;i < 1000;i++)
-        printf("%f",(double)clock()/CLOCKS_PER_SEC);
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    int i, j, k;
+    srand(time(0));
+    for(i=0; i<1000; i++){
+        k = rand()%100;
+        printf("%3d\n", k);
+        if(k > 50)
+            j++;
+    }
+    printf("%d\n", j);
     return 0;
 }
