406588f934ad6ce734423b47246ec136647b8e09
Macpotty <645092284@qq.com>
2016-03-19 20:02:09 +0800

    update

--- practices/test.c
+++ practices/test.c
@@ -1,16 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Class{
+    int a;
+    int (*generator)(struct Class *this);
+}Class;
+
+
+int func(struct Class *this){
+    this->a++;
+    printf("%d", this->a);
+}
+
+Class *ClassCtreator(){
+    Class *this;
+    this = (Class*)malloc(sizeof(Class));
+    this->a = 0;
+    this->generator = func;
+    return this;
+}
+
 int main()
 {
-    int i, j, k;
-    srand(time(0));
-    for(i=0; i<1000; i++){
-        k = rand()%100;
-        printf("%3d\n", k);
-        if(k > 50)
-            j++;
-    }
-    printf("%d\n", j);
-    return 0;
+    Class *p = ClassCtreator();
+    p->generator(p);
+    printf("%d", p->a);
 }
