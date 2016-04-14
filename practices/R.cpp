1ba0ae6aeb80c9bec8bf4a50304158ef4f07e7dc
Macpotty <645092284@qq.com>
2016-04-14 14:03:12 +0800

    Duck's homework

--- practices/R.cpp
+++ practices/R.cpp
@@ -3,22 +3,23 @@ using namespace std;
 char *findC (const char *source, const char *obj) {
     const char *pointer = source;
     const char *objcpy = obj;
-    while (*(pointer++) != '\0') {
-        while (*(pointer) == *(objcpy)) {
+    while (*pointer != '\0') {
+        while (*pointer == *objcpy) {
             pointer++;
             objcpy++;
-            if (*(pointer) != *(objcpy) && *objcpy != '\0')
+            if (*pointer != *objcpy && *objcpy != '\0')
                 return NULL;
         }
         if(*objcpy == '\0')
         	return (char *)obj;
+        pointer++;
     }
     return NULL;
 }
 
 int main (){
     const char *source = "ABCDEAB";
-    const char *obj = "AB";
+    const char *obj = "ABCD";
     char *result = findC(source, obj);
     cout << result << endl;
     return 0;
