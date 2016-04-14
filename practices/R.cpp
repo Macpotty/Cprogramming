2b6f543a16550ea19e033a93c97d790dab52bed4
Macpotty <645092284@qq.com>
2016-04-14 13:57:49 +0800

    Duck's homework

---
+++ practices/R.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+using namespace std;
+char *findC (const char *source, const char *obj) {
+    const char *pointer = source;
+    const char *objcpy = obj;
+    while (*(pointer++) != '\0') {
+        while (*(pointer) == *(objcpy)) {
+            pointer++;
+            objcpy++;
+            if (*(pointer) != *(objcpy) && *objcpy != '\0')
+                return NULL;
+        }
+        if(*objcpy == '\0')
+        	return (char *)obj;
+    }
+    return NULL;
+}
+
+int main (){
+    const char *source = "ABCDEAB";
+    const char *obj = "AB";
+    char *result = findC(source, obj);
+    cout << result << endl;
+    return 0;
+}
--- test1/test1.cpp
+++ test1/test1.cpp
@@ -1,67 +1,25 @@
-#include <iostream>
-#include <string>
-
-struct State
-{
-    State (std::string topermute_, int place_, int nextchar_, State* next_ = 0)
-            : topermute (topermute_)
-            , place (place_)
-            , nextchar (nextchar_)
-            , next (next_)
-    {
+#include "iostream"
+using namespace std;
+char *findC (const char *source, const char *obj) {
+    const char *pointer = source;
+    const char *objcpy = obj;
+    while (*(pointer++) != '\0') {
+        while (*(pointer) == *(objcpy)) {
+            pointer++;
+            objcpy++;
+            if (*(pointer) != *(objcpy) && *objcpy != '\0')
+                return NULL;
+        }
+        if(*objcpy == '\0')
+            return (char *)obj;
     }
-
-    std::string topermute;
-    int place;
-    int nextchar;
-
-    State* next;
-};
-
-std::string swtch (std::string topermute, int x, int y)
-{
-    std::string newstring = topermute;
-    newstring[x] = newstring[y];
-    newstring[y] = topermute[x]; //avoids temp variable
-
-    return newstring;
+    return NULL;
 }
 
-void permute (std::string topermute, int place = 0)
-{
-    // Linked list stack.
-    State* top = new State (topermute, place, place);
-
-    while (top != 0)
-    {
-        State* pop = top;
-        top = pop->next;
-
-        if (pop->place == pop->topermute.length () - 1)
-        {
-            std::cout << pop->topermute << std::endl;
-        }
-
-        for (int i = pop->place; i < pop->topermute.length (); ++i)
-        {
-            top = new State (swtch (pop->topermute, pop->place, i), pop->place + 1, i, top);
-        }
-
-        delete pop;
-    }
-}
-
-int main (int argc, char* argv[])
-{
-    if (argc!=2)
-    {
-        std::cout<<"Proper input is 'permute string'";
-        return 1;
-    }
-    else
-    {
-        permute (argv[1]);
-    }
-
+int main (){
+    const char *source = "ABCDEAB";
+    const char *obj = "AB";
+    char *result = findC(source, obj);
+    cout << result << endl;
     return 0;
 }
